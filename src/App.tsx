b9a00c241e3db4a5cd1118e87f0784b0c1e88617
hybridx <hybridx18@gmail.com>
2023-09-18 03:36:24 +0530

    Added basic code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { usePDF } from 'react-to-pdf';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState(null);
+  const [amount, setAmount] = useState(null);
+  const { toPDF, targetRef } = usePDF({ filename: 'page.pdf' });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onChange = (event: any) => {
+    if (event.target.id === 'name') {
+      setName(event.target.value);
+    }
+    if (event.target.id === 'amount') {
+      setAmount(event.target.value);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid place-items-center">
+      <div className='mt-5'>Add your input</div>
+      <form
+        className="border rounded-md m-5 p-5 bg-gray-100"
+        onSubmit={(event) => {
+          event.preventDefault();
+          toPDF();
+        }}
+      >
+        <div className="m-2 p-2">
+          <label
+            className="block mb-2 text-sm font-medium text-gray-900"
+            htmlFor="name"
+          >
+            Please enter name:{' '}
+          </label>
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="name"
+            onChange={onChange}
+          />
+        </div>
+        <div className="m-2 p-2">
+          <label
+            className="block mb-2 text-sm font-medium text-gray-900"
+            htmlFor="amount"
+          >
+            Please enter amount:{' '}
+          </label>
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            type="text"
+            pattern="\d{5}"
+            name="amount"
+            id="amount"
+            placeholder="100"
+            onChange={onChange}
+          />
+        </div>
+        <div className="m-2 p-2">
+          <button
+            type="submit"
+            className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            Created PDF
+          </button>
+        </div>
+      </form>
+      <div className="m-4 p-4 border" ref={targetRef}>
+        <h1>
+          This will be converted to pdf {name} with amount {amount}
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
